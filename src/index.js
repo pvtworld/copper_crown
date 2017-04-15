25fdbd8d5032c03071fc4ba919a72fc74a8451fd
nij0 <niclas.j.jonsson@gmail.com>
2017-04-15 15:41:57 +0200

    Cleaned up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, IndexRoute, hashHistory} from 'react-router';
-import App from './App';
 import CopperMap from './Components/CopperMap'
 import Login from './Components/Login'
 import './index.css';
