70c524e446c3763a1fb78b5911ca99d2f8c490c5
nij0 <niclas.j.jonsson@gmail.com>
2017-04-15 18:52:48 +0200

    GitHub Pages doesnt suppoert browserHistory (nice URL)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
 import './index.css';
-import {Route, Router, IndexRoute, browserHistory} from 'react-router';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
 import CopperMap from './Components/CopperMap'
 import Login from './Components/Login'
+import NotFound from './Components/NotFound'
 
 const root = document.getElementById('root');
-const routes = <Router history={browserHistory}>
+const routes = <Router history={hashHistory}>
     <Route path="/">
         <Route path="coppermap" component={CopperMap}/>
         <Route path="main" component={Main}/>
         <IndexRoute component={Login}/>
+        <Route path="*" component={NotFound} />
     </Route>
 </Router>;
 
