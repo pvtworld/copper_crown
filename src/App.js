74c9f7888b9aad17b886728a01d955f9d79ce6a9
nij0 <niclas.j.jonsson@gmail.com>
2017-04-21 09:21:48 +0200

    Removed home.jsx and path to it

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import CopperMap from './Components/CopperMap'
 import Login from './Components/Login'
-import Home from './Components/Home'
+
 
 const App = () => {
   return (
       <Router>
         <div>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login}/>
+            <Route exact path="/" component={Login}/>
             <Route path="/coppermap" component={CopperMap}/>
         </div>
       </Router>
