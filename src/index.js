96b57aad4bff93ec06e01cf0bc7b946d46cc8520
nij0 <niclas.j.jonsson@gmail.com>
2017-04-23 16:58:59 +0200

    removed bootstrap.css and replaced with link in index.html.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import './index.css';
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 const root = document.getElementById('root');
 
