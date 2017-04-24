cf9aafb3b0a9f439e53048866fe43396fbb64929
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-04-24 15:32:52 +0200

    Added geolocation

---
+++ src/Helpers/GeoHelpers.js
@@ -0,0 +1,10 @@
+// Get location from device
+export const geolocation = (
+    navigator.geolocation ?
+        navigator.geolocation :
+        ({
+            getCurrentPosition(success, failure) {
+                failure(`Your browser doesn't support geolocation.`);
+            },
+        })
+);
