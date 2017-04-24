ea70c1a753f08afb021d38a41776f4bf8bc4c9d2
nij0 <niclas.j@bktv.nu>
2017-04-24 23:17:05 +0200

    Removed components which had nothing to do with login feature

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
-import React from 'react'
-import {Route, Redirect, Switch, BrowserRouter} from 'react-router-dom';
-import CopperMap from './Components/CopperMap';
-import Login from './Components/Login';
-import Dashboard from './Components/Dashboard';
-import firebase from './Firebase/firebase';
-import Welcome from './Components/Welcome';
+import React, { Component } from 'react';
+import './App.css';
+import CopperMap from './Components/CopperMap'
 
-function PrivateRoute ({component: Component, authed, ...rest}) {
-    return (
-        <Route
-            {...rest}
-            render={(props) => authed === true
-                ? <Component {...props} />
-                : <Redirect to={{pathname: '/', state: {from: props.location}}} />}
-        />
-    )
-}
-
-function PublicRoute ({component: Component, authed, ...rest}) {
-    return (
-        <Route
-            {...rest}
-            render={(props) => authed === false
-                ? <Component {...props} />
-                : <Redirect to='/dashboard' />}
-        />
-    )
-}
-
-export default class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            authed: false
-        }
-    }
-    componentDidMount () {
-        this.removeListener = firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.setState({
-                    authed: true
-                });
-                console.log('User IS auth');
-            } else {
-                this.setState({
-                    authed: false
-                });
-                console.log('User NOT auth');
-            }
-        })
-    }
-    componentWillUnmount () {
-        this.removeListener()
-    }
+class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                        <Switch>
-                            <Route path='/' exact component={Welcome} />
-                            <PublicRoute authed={this.state.authed} path='/login' component={Login} />
-                            <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} />
-                            <PrivateRoute authed={this.state.authed} path='/coppermap' component={CopperMap} />
-                            <Route render={() => <h3>404 feterror</h3>} />
-                        </Switch>
-            </BrowserRouter>
+            <CopperMap/>
         );
     }
 }
+export default App;
