0e35c2db182bf87cb4b26bb7f54968152462d0b1
nij0 <niclas.j@bktv.nu>
2017-04-25 12:34:07 +0200

    Created GameContainer file

---
+++ src/Components/GameContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './Component/Navbar';
+
+
+class GameContainer extends React.Component{
+
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default GameContainer;
