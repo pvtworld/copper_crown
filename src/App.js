513e9c96459017dd902177342b76968e82352c3e
nij0 <niclas.j@bktv.nu>
2017-04-25 09:06:22 +0200

    import login to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import CopperMap from './Components/CopperMap'
+//import Login from './Components/Login'
 
 class App extends Component {
     render() {
