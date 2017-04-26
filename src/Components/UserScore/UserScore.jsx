d16396395dd78fe5e2374b81b054dd3cde893c78
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-04-26 18:16:58 +0200

    Removed console log

--- src/Components/UserScore/UserScore.jsx
+++ src/Components/UserScore/UserScore.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default class UserScore extends React.Component {
     render(){
-    return(
-        <div>
-            <h1>User Score is: {this.props.state.userInfo.points}</h1>
-        </div>
-    );
+        return(
+            <div>
+                <h1>User Score is: {this.props.state.userInfo.points}</h1>
+            </div>
+        );
     }
 };
