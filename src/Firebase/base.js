45c0f126e4573e52e4eaa884307e93161212e0ac
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-04-26 14:28:30 +0200

    Added firebase credentials

---
+++ src/Firebase/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBxf-SMmqau97RBu5y4f4a62OQBWLwevUM",
+    authDomain: "coppercrown-a18fd.firebaseapp.com",
+    databaseURL: "https://coppercrown-a18fd.firebaseio.com/",
+});
+
+export default base;
