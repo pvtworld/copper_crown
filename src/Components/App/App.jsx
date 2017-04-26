6d317c74c14b7db4dcca0dece42b6e93dbdf6c7b
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-04-26 14:38:39 +0200

    Added Firebase

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -5,31 +5,66 @@ import firebase from '../../Firebase/firebase';
 export default class App extends React.Component {
     constructor(){
         super();
-        this.state = {
-            authed: false
+        this.addPoints = this.addPoints.bind(this);
+    }
+
+    state = {
+        authed: false,
+        userInfo : {
+            points: 0,
         }
     }
-    componentDidMount () {
+
+    componentWillMount() {
         this.removeListener = firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 this.setState({
                     authed: true
                 });
+
+                this.ref = base.syncState(`users/${user.uid}`, {
+                    context: this,
+                    state: 'userInfo'
+                });
+
                 console.log('User IS auth');
             } else {
                 this.setState({
                     authed: false
                 });
-                console.log('User NOT auth');
+                console.log("User is NOT auth")
             }
         })
     }
+
+
+    componentDidMount () {
+    }
+
+
     componentWillUnmount () {
         this.removeListener()
+        this.removeListener();
+        base.removeBinding(this.ref);
+
     }
+
+    addPoints(newPoints) {
+        console.log("points is:")
+        console.log(newPoints);
+
+        const userInfo= {...this.state.userInfo};
+        userInfo.points += newPoints;
+        // set state
+        this.setState({ userInfo });
+    }
+
     render() {
         return (
-            <CopperMap/>
+            <CopperMap
+                state={this.state}
+                addPoints={this.addPoints}
+            />
         );
     }
 }
