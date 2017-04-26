4009f5c6d103bb537f777b4e3acc308f8ef589b7
Ludvig <ludvig.lundberg@hotmail.com>
2017-04-26 23:51:06 +0200

    Updated the test in copperMap and added a snapshot!

--- tests/Components/CopperMap.test.js
+++ tests/Components/CopperMap.test.js
@@ -1,22 +1,19 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import CopperMap from '../../src/Components/CopperMap/CopperMap'
 
 beforeAll(() => {
     
 });
 describe('Render test', ()=> {
-    it('Renders without crashing', () => {
-        const state = {
-        authed: false,
-        userInfo : {
-            points: 0,
-            }
-        }
-
-        const component = renderer.create(<CopperMap state={state} />);
-
-        expect(component).toMatchSnapshot();
-    });
+    it('renders a GameMap', () => {
+        const component = shallow(<CopperMap/>);
+        component.setState({authed : true});
+        component.setState({userInfo: {points : 0}});
+        
+        expect(toJson(component)).toMatchSnapshot();
+    })
 });
