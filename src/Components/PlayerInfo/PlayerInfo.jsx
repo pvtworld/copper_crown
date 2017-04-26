a5ce6e25ed76172aaf22ff37df214d8a00561520
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2017-04-26 22:12:38 +0200

    added PlayerInfo-component

---
+++ src/Components/PlayerInfo/PlayerInfo.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Grid, Col, Row} from 'react-bootstrap';
+
+export default class PlayerInfo extends Component {
+
+
+    render() {
+
+        return (
+
+            <div className="container">
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={1} md={2} className="text-right"><strong>Player:</strong></Col>
+                        <Col xs={2} md={3} className="text-left">{this.props.player}</Col>
+                    </Row>
+
+                    <Row className="show-grid">
+                        <Col xs={1} md={2} className="text-right"><strong>Total Value:</strong></Col>
+                        <Col xs={2} md={3} className="text-left">{this.props.totalValue}</Col>
+                    </Row>
+
+                    <Row className="show-grid">
+                        <Col xs={1} md={2} className="text-right"><strong>Total Area:</strong></Col>
+                        <Col xs={2} md={3} className="text-left">{this.props.totalArea}</Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+
+
+}
+
+
