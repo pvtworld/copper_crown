afca61e6e9a8f4d589631c90060e13c7b70c801d
nij0 <niclas.j@bktv.nu>
2017-04-27 23:03:09 +0200

    Played around with css. The login box is not responsive what so ever
    Similar must be possible to create with Bootstrap instead

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,12 +5,15 @@ import './Login.css'
 export default class Login extends React.Component {
     render() {
         return (
-            <div className="login">
-                <h2>CopperCrown - 0.1</h2>
-                <p>Sign in to play the Game</p>
-                <Button bsStyle="primary" bsSize="large" block onClick={() => this.props.authenticate('facebook')} >Log In with Facebook</Button>
-                <Button bsStyle="default" bsSize="large" block onClick={() => this.props.authenticate('github')}>Log In with Github</Button>
-                <Button bsStyle="danger" bsSize="large" block onClick={() => this.props.authenticate('google')} >Log In with Google</Button>
+            <div className="box">
+                <div className="login">
+                    <h2>CopperCrown - 0.1</h2>
+                    <hr />
+                    <p>Sign in to play the Game</p>
+                    <Button bsStyle="primary" bsSize="large" block onClick={() => this.props.authenticate('facebook')} >Log In with Facebook</Button>
+                    <Button bsStyle="default" bsSize="large" block onClick={() => this.props.authenticate('github')}>Log In with Github</Button>
+                    <Button bsStyle="danger" bsSize="large" block onClick={() => this.props.authenticate('google')} >Log In with Google</Button>
+                </div>
             </div>
         );
     }
