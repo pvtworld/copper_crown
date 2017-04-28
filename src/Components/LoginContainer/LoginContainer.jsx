2a686a6ccb72ccdc81e076a71b8a1af9762eb286
nij0 <niclas.j@bktv.nu>
2017-04-28 11:50:53 +0200

    Added headings

--- src/Components/LoginContainer/LoginContainer.jsx
+++ src/Components/LoginContainer/LoginContainer.jsx
@@ -6,6 +6,10 @@ export default class LoginContainer extends React.Component {
     render() {
         return (
             <div>
+                <div id="center_text">
+                    <h2>COPPER</h2>
+                    <h3>CROWN</h3>
+                </div>
                 <Login authenticate={this.props.authenticate}/>
             </div>
         );
