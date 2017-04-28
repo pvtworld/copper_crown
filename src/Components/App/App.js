073a57efd73b37436390485f254791c742b6e293
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-04-28 22:45:27 +0200

    bind roofAlreadyStolen

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,6 +11,7 @@ export default class App extends React.Component {
         this.logout = this.logout.bind(this);
         this.authHandler = this.authHandler.bind(this);
         this.addRoof = this.addRoof.bind(this);
+        this.roofAlreadyStolen = this.roofAlreadyStolen.bind(this);
 
     }
 
