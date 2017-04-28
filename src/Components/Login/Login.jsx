efa69e35dc6e16d2dea7d232fc996f18a5f92a9b
nij0 <niclas.j@bktv.nu>
2017-04-28 11:58:29 +0200

    manages to get the css right for multiple heading on the same line and center them

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -7,9 +7,9 @@ export default class Login extends React.Component {
         return (
             <div className="box">
                 <div className="login">
-                    <h2>CopperCrown - 0.1</h2>
-                    <hr />
                     <p>Sign in to play the Game</p>
+                    <hr />
+
                     <Button bsStyle="primary" bsSize="large" block onClick={() => this.props.authenticate('facebook')} >Log In with Facebook</Button>
                     <Button bsStyle="default" bsSize="large" block onClick={() => this.props.authenticate('github')}>Log In with Github</Button>
                     <Button bsStyle="danger" bsSize="large" block onClick={() => this.props.authenticate('google')} >Log In with Google</Button>
