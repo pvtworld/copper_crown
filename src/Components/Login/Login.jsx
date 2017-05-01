8779d548d3b393c4ffc8878b6382c0b59d63c5c2
nij0 <niclas.j@bktv.nu>
2017-05-01 15:21:14 +0200

    Replaced html with react-boostrap

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Panel, Col} from 'react-bootstrap';
 import './Login.css'
+
+const title = <h2>Sign in to play the game</h2>;
 
 export default class Login extends React.Component {
     render() {
         return (
-            <div className="box">
-                <div className="login">
-                    <p>Sign in to play the Game</p>
-                    <hr />
-
+            <Col md={4} mdOffset={4}>
+                <Panel className="login" header={title}>
                     <Button bsStyle="primary" bsSize="large" block onClick={() => this.props.authenticate('facebook')} >Log In with Facebook</Button>
                     <Button bsStyle="default" bsSize="large" block onClick={() => this.props.authenticate('github')}>Log In with Github</Button>
                     <Button bsStyle="danger" bsSize="large" block onClick={() => this.props.authenticate('google')} >Log In with Google</Button>
-                </div>
-            </div>
+                </Panel>
+
+            </Col>
         );
     }
 };
-
-
 
 
