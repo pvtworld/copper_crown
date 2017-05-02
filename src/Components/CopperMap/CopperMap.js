1232724dc464aeda12434779314e47fd382e4786
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-05-02 15:30:54 +0200

    Created GameMap component

--- src/Components/CopperMap/CopperMap.js
+++ src/Components/CopperMap/CopperMap.js
@@ -1,63 +1,7 @@
 import React, {Component} from 'react';
-import {withGoogleMap, GoogleMap, Circle, Marker} from "react-google-maps";
 import {geolocation, checkClickForCopper} from '../../Helpers/GeoHelpers';
 import InfoContainer from "../InfoContainer/InfoContainer";
-
-const GameMap = withGoogleMap(props => (
-
-
-    <GoogleMap
-        center={props.center}
-        defaultZoom={17}
-        defaultCenter={props.center}
-        onClick={props.onMapClick}
-        options={{
-            mapTypeControl: false,
-            streetViewControl: false,
-            // zoomControl: false,
-            //draggable: false,
-            //scrollwheel: false,
-            //panControl: false,
-            //maxZoom: 18,
-            //minZoom: 18,
-            //zoom: 18,
-        }}
-
-    >
-
-
-
-        <Circle
-            onClick={(event => (
-                checkClickForCopper(event.latLng.lng(), event.latLng.lat(), props.handleRoof)
-            ))
-            }
-            center={props.center}
-            radius={100}
-            options={{
-                fillColor: `green`,
-                fillOpacity: 0.10,
-                strokeColor: `black`,
-                strokeOpacity: 0.50,
-                strokeWeight: 2,
-            }}
-        />
-
-        <Marker
-            defaultPosition={props.center}
-            position={props.center}
-            title="Click to zoom"
-            icon={{url: '/images/racoon.png',
-                scaledSize: {width: 20, height: 20},
-                size: {width: 360, height: 330},
-                anchor: {x:10, y:10}
-
-            }}
-            onClick={props.onMarkerClick}
-        />
-
-    </GoogleMap>
-));
+import {GameMap} from "../GameMap/GameMap";
 
 export default class CopperMap extends Component {
     constructor() {
