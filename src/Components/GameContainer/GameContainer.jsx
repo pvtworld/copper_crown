8e6044a6af199616b7ad0b25f0321e76fdf86e16
nij0 <niclas.j@bktv.nu>
2017-05-02 20:44:53 +0200

    routing with state in GameContainer

--- src/Components/GameContainer/GameContainer.jsx
+++ src/Components/GameContainer/GameContainer.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import CopperMap from '../CopperMap/CopperMap'
+import InfoComponent from '../Info/InfoComponent'
+import {Navbar, NavItem, Nav} from 'react-bootstrap'
 
 export default class GameContainer extends React.Component{
+    state = {
+        renderInfo: false,
+        renderLeaderboard: false,
+        renderProfile: false
+    };
+
+    renderInfo = () => {
+        this.setState({
+            renderInfo: true
+        })
+    };
+
+    leaveInfo = () => {
+        this.setState({
+            renderInfo: false
+        })
+    };
+
     render() {
         const logout = <button onClick={this.props.logout}>Log Out!</button>;
         const leaderboard = <button onClick={this.props.getLeader}>Print Leaderboard in console</button>;
+        const info = <button onClick={this.renderInfo}>Info</button>;
+
+        if(this.state.renderInfo){
+            return(<InfoComponent leaveInfo={this.leaveInfo}/>)
+        }
+
         return (
             <div>
                 <CopperMap state={this.props.state}
                            addRoof={this.props.addRoof}
                            roofAlreadyStolen={this.props.roofAlreadyStolen}
                 />
-                {logout}{leaderboard}
+                {logout}{leaderboard}{info}
             </div>
         )
     }
