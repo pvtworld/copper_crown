fb448a824a07800519d3caedbf7d0b6127b78225
Tobias Ahnhem <tobias.ahnhem@gmail.com>
2017-05-03 23:34:09 +0200

    commented out unused function, but could be used later on

--- src/Helpers/PointsHelpers.js
+++ src/Helpers/PointsHelpers.js
@@ -20,38 +20,38 @@ export function getPricePerSquareMeter(setPricePerSquareMeter) {
                 .then((jsonObject) => {
                     currentRate = jsonObject.dataset.data[0][1];
                     setPricePerSquareMeter((currentPrice * currentRate) / squareMetersPerTon);
-                }).catch((error) =>{
+                }).catch((error) => {
                 alert('Error: Please refresh page.');
             });
         });
 }
 
-function status(response){
-    if(response.status === 200){
+function status(response) {
+    if (response.status === 200) {
         return Promise.resolve(response);
     }
     return Promise.reject(new Error(response.statusText));
 }
 
-function json(response){
+function json(response) {
     return response.json();
 }
 
 function currentDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    var month = today.getMonth() + 1;
-    var day = today.getDate();
-
-    if (month.toString().length === 1) {
-        month = '0' + month;
-    }
-
-    if (day.toString().length === 1) {
-        day = '0' + day;
-    }
-
-    //return year + '-' + month + '-' + day;
+    // const today = new Date();
+    // const year = today.getFullYear();
+    // var month = today.getMonth() + 1;
+    // var day = today.getDate();
+    //
+    // if (month.toString().length === 1) {
+    //     month = '0' + month;
+    // }
+    //
+    // if (day.toString().length === 1) {
+    //     day = '0' + day;
+    // }
+    //
+    // return year + '-' + month + '-' + day;
 
     return '2017-05-02';
 }
