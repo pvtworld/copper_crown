e8a2d2f9f505fd3b78d998170b97368b8c25bb5b
tiliacordata <tilia_cordata@outlook.com>
2017-05-03 13:14:53 +0200

    Cleaned up code

--- src/Components/LeaderboardPage.jsx
+++ src/Components/LeaderboardPage.jsx
@@ -33,8 +33,7 @@ export default class LeaderboardPage extends Component{
         }
         return listItems;
     }
-
-
+    
     render(){
         return (
             <Grid>
@@ -44,24 +43,4 @@ export default class LeaderboardPage extends Component{
             </Grid>
         );
     }
-
-    /*render(){
-        return (
-            <Grid>
-                <LeaderHeading/>
-                <LeaderboardItem pos={this.getNewPos()} name="Agata Agatasson" points="9000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Bertil Bertilsson" points="8000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Cyntia Cyntiasson" points="7000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Dieter Dietersson" points="6000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Elna Elnasson" points="5000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Fredde Freddesson" points="4000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Greta Gretasson" points="3000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Henry Henrysson" points="2000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Ida Idasson" points="1000" />
-                <LeaderboardItem pos={this.getNewPos()} name="Julius Juliusson" points="500" />
-                <CurrentRank rank="847" />
-            </Grid>
-        );
-    }*/
-
 }
