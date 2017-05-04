690e10fb3c90080bc9ce8cbc8ae5072ec2e58bfb
nij0 <niclas.j@bktv.nu>
2017-05-04 11:37:56 +0200

    Added text in mobile menu mode

--- src/Components/Navigation/Navigationbar.jsx
+++ src/Components/Navigation/Navigationbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 export default class Navigationbar extends React.Component{
     render(){
         return(
             <Navbar inverse collapseOnSelect>
                 <Navbar.Header>
+                    <a href ="#" className="navbar-text visible-xs-inline-block">Example text</a>
                     <Navbar.Brand>
                         <a>CopperCrown</a>
                     </Navbar.Brand>
@@ -14,11 +15,18 @@ export default class Navigationbar extends React.Component{
                 <Navbar.Collapse>
                     <Nav>
                         <NavItem eventKey={1} onClick={this.props.renderProfile}>Profile</NavItem>
-                        <NavItem eventKey={2} onClick={this.props.renderLeader}>Leaderboards</NavItem>
-                        <NavItem eventKey={3} onClick={this.props.renderInfo}>Info</NavItem>
                         <NavItem eventKey={4} onClick={this.props.renderPlayerInfo}>PlayerInfo</NavItem>
+                        <NavItem eventKey={2} onClick={this.props.renderLeader}>Leaderboards</NavItem>
+                        <NavItem eventKey={3} onClick={this.props.renderInfo}>About</NavItem>
                     </Nav>
                     <Nav pullRight>
+                        <NavDropdown eventKey={3} title="Signed in as: " id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                        </NavDropdown>
                         <NavItem onClick={this.props.logout}>Logout</NavItem>
                     </Nav>
                 </Navbar.Collapse>
