ba5163491b98c73a67594cbcb2a30aa399683aba
MoaEkberg <chipanmoa@hotmail.com>
2017-05-08 11:28:41 +0200

    new code pasted

--- src/Components/Profile/ProfileComponent.jsx
+++ src/Components/Profile/ProfileComponent.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Button} from 'react-bootstrap'
-
+import './ProfileComponent.css'
+import {Button, Image} from 'react-bootstrap'
 export default class ProfileComponent extends React.Component{
     render(){
         const back = <Button bsStyle="primary" onClick={this.props.leaveProfile}>Close</Button>;
-
+        var {name, points, userID, roofsTaken, roofSizeTaken}=this.props;
         if(this.props.renderProfile){
             return(
                 <div className="navpage-box">
                     <h1>Profile Component</h1>
                     {back}
+                    <div id="center_text">
+                    <h1>My account</h1>
+                    <h4>Logged in as: {name} </h4>
+                    <h5>User ID:  {userID}</h5>
+                    <Image src="/anonympers.jpg" responsive />
+                    <h5>Mail: </h5>
+                    <h5>Points: {points}</h5>
+                    <h5>Number of roofs taken: {roofsTaken}</h5>  
+                    <h5>Area of roofs taken:  {roofSizeTaken}</h5>
+                    </div>
                 </div>
             )
         }
