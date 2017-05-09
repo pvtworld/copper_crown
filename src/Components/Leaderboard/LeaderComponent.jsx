e2f5b529daeb103fc23bf65353a10e3188c7e61f
tiliacordata <tilia_cordata@outlook.com>
2017-05-09 15:22:56 +0200

    Refactored to Redux

--- src/Components/Leaderboard/LeaderComponent.jsx
+++ src/Components/Leaderboard/LeaderComponent.jsx
@@ -1,25 +1,53 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {firebaseConnect, dataToJS, pathToJS} from 'react-redux-firebase';
+import {Grid, Button} from 'react-bootstrap';
 import LeaderHeading from './LeaderHeading';
 import LeaderboardList from './LeaderboardList';
 import CurrentRank from './CurrentRank';
-import {Grid, Button} from 'react-bootstrap';
 
-export default class LeaderComponent extends Component{
+class LeaderComponent extends Component{
 
-    constructor (){
+    constructor(){
         super();
-        this.listCallback = this.listCallback.bind(this);
-        this.state = {
-            leaderboard: [],
-            currentRank: undefined
-        };
+        this.getUserInfo = this.getUserInfo.bind(this);
+        this.getMyRank = this.getMyRank.bind(this);
+        this.getLeaderboardInfo = this.getLeaderboardInfo.bind(this);
     }
 
-    listCallback (leaderbordInfo, rank) {
-        this.setState({
-            leaderboard: leaderbordInfo,
-            currentRank: rank
-        });
+    getUserInfo() {
+        var unsortedUserInfo = this.props.users;
+        var idArray = Object.keys(unsortedUserInfo);
+        var dataArray = Object.values(unsortedUserInfo);
+        for (var i = 0; i < dataArray.length; i++){
+            dataArray[i].userId=idArray[i];
+        }
+        return dataArray.sort(function(a, b) {return b.points - a.points});
+    };
+
+    getMyRank(sortedUserInfo){
+        var myRank = undefined;
+        for (var i = 0; i < sortedUserInfo.length; i++){
+            if (sortedUserInfo[i].userId === this.props.auth.uid){
+                myRank = i + 1;
+                break;
+            }
+        }
+        return myRank;
+    }
+
+    getLeaderboardInfo(sortedUserInfo){
+        var listItems = [];
+        var i = 1;
+        while (i <= sortedUserInfo.length && i <= 10){
+            listItems.push({
+                pos: i,
+                name: sortedUserInfo[i-1].userId,
+                points: sortedUserInfo[i-1].points
+            });
+            i++;
+        }
+        return listItems;
     }
 
     render(){
@@ -28,7 +56,9 @@ export default class LeaderComponent extends Component{
 
         if(this.props.renderLeader){
 
-            this.props.getLeaderboard(this.listCallback);
+            var sortedUserInfo = this.getUserInfo();
+            var myRank = this.getMyRank(sortedUserInfo);
+            var listItems = this.getLeaderboardInfo(sortedUserInfo);
 
             return(
                 <div className="navpage-box">
@@ -39,8 +69,8 @@ export default class LeaderComponent extends Component{
                     <div>
                         <Grid>
                             <LeaderHeading/>
-                            <LeaderboardList listItems={this.state.leaderboard} />
-                            <CurrentRank rank={this.state.currentRank} />
+                            <LeaderboardList listItems={listItems} />
+                            <CurrentRank rank={myRank} />
                         </Grid>
                     </div>
                 </div>
@@ -49,3 +79,14 @@ export default class LeaderComponent extends Component{
         return null;
     }
 }
+
+var wrappedUserInfo = firebaseConnect(
+    ['/users']
+)(LeaderComponent);
+
+export default connect(
+    ({firebase}) => ({
+        users: dataToJS(firebase, 'users'),
+        auth: pathToJS(firebase, 'auth')
+    })
+)(wrappedUserInfo);
