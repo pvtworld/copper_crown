568fe2527f4a3521e31b1c4ed48fe4f789cb539d
tiliacordata <tilia_cordata@outlook.com>
2017-05-09 21:14:04 +0200

    Added new reducers to combiner

--- src/Redux/Reducers/combiner.jsx
+++ src/Redux/Reducers/combiner.jsx
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { firebaseStateReducer } from 'react-redux-firebase'
+import { copperSearchReducer, displayRoofReducer } from './copperMapReducers';
 
 const reducers = combineReducers({
-    firebase: firebaseStateReducer
-})
+    firebase: firebaseStateReducer,
+    searchingForCopper: copperSearchReducer,
+    displayRoofInfo: displayRoofReducer
+});
 
 export default reducers;
