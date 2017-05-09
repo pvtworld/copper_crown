2e608da25586bf89bd76f2204d625b22c3999763
Ludvig Lundberg <ludvig.lundberg@hotmail.com>
2017-05-09 10:49:42 +0200

    Removed unnecesary reducers and actions

--- src/Redux/Actions/loginAction.jsx
+++
@@ -1,8 +0,0 @@
-export const loginAction =   {
-        type: "DISPATCH_USER_LOGIN", 
-        item: "facebook"
-}
-
-export const logoutAction = {
-    type: "USER_LOGOUT"
-}
--- src/Redux/Reducers/combiner.jsx
+++ src/Redux/Reducers/combiner.jsx
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 import { firebaseStateReducer } from 'react-redux-firebase'
-import user from './userReducer';
 
 const reducers = combineReducers({
-    user,
     firebase: firebaseStateReducer
 })
 
