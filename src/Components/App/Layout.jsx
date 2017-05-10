5b692a35e7448fe059943165012cae3438598bf3
nij0 <niclas.j@bktv.nu>
2017-05-10 11:16:03 +0200

    Created Layout for future purposes

---
+++ src/Components/App/Layout.jsx
@@ -0,0 +1,12 @@
+/*
+import React from 'react'
+import Nav from '../Navigation/Navigationbar'
+
+var Layout = (props) => (
+    <div>
+        <Nav/>
+        {props.children}
+    </div>
+)
+
+export default Layout;*/
