54351147016e159a99d40962a94aa53d29ba4b80
Ludvig <ludvig.lundberg@hotmail.com>
2017-05-11 18:59:00 +0200

    Added reducers

--- src/Redux/Reducers/copperMapReducers.jsx
+++ src/Redux/Reducers/copperMapReducers.jsx
@@ -1,16 +1,26 @@
-export var copperSearchReducer = (state = false, action) => {
+export var copperSearchReducer = (state = {searching: false}, action) => {
     switch (action.type){
         case 'SEARCHING_FOR_COPPER':
-            return action.searching;
+            return {...state, searching: true};
+        case 'COPPER_SEARCH_DONE':
+            return {...state, searching: false};
         default:
             return state;
     }
 };
 
-export var displayRoofReducer = (state = false, action) => {
+
+
+export var displayRoofReducer = (state = {foundRoof: null}, action) => {
     switch (action.type){
-        case 'DISPLAY_ROOF_INFO':
-            return true;
+        case 'DISPLAY_ROOF_NOT_TAKEN':
+            return {...state, foundRoof: true, roofTaken: false, id: action.id, area: action.area};
+        case 'DISPLAY_ROOF_TAKEN':
+            return {...state, foundRoof: true, roofTaken: true, id: action.id, area: action.area};
+        case 'DISPLAY_ROOF_NOT_FOUND':
+            return {...state, foundRoof: false}
+        case 'RESET_ROOF':
+            return {...state, foundRoof: null}
         default:
             return state;
     }
