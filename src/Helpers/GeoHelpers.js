ca5aca2aaa29eabd2769223d0f30cdf8afc8446e
Ludvig <ludvig.lundberg@hotmail.com>
2017-05-11 18:58:48 +0200

    remade checkClickForCopper

--- src/Helpers/GeoHelpers.js
+++ src/Helpers/GeoHelpers.js
@@ -1,4 +1,6 @@
 import { convertPoint } from './CoordinateConverter';
+import {searchForCopper, searchDone} from '../Redux/Actions/copperMapActions';
+import { connect } from 'react-redux';
 
 export const geoError = (err) => {
     console.warn('ERROR(' + err.code + '): ' + err.message);
@@ -11,55 +13,42 @@ export const geoOptions = {
 }
 
 
-export const checkClickForCopper = (long, lat) => {
+const convertFromTextToXML = (textToConvert) => {
+        const parser = new DOMParser();
+        const textToReturn = parser.parseFromString(textToConvert, 'text/xml');
+        return (textToReturn);
+}
+
+const createObjectFromXML = (xml) => {
+    return{
+        id: xml.getElementsByTagName('id')[0].childNodes[0].nodeValue,
+        area: xml.getElementsByTagName('area')[0].childNodes[0].nodeValue 
+        }
+}
+
+
+export const checkClickForCopper = (long, lat, dispatch, firebase) => {
 
     const sthlmPointUrlTemplate = 'https://us-central1-coppercors.cloudfunctions.net/copperProvider/?whatcopper=Geom=POINT(%longitude%%20%latitude%)';
     const [x,y] = convertPoint(long, lat);
+    dispatch(searchForCopper());
 
     fetch(createUrl(sthlmPointUrlTemplate, x, y), { method: 'GET'} )
+    .then( response => response.status === 200 ? Promise.resolve(response) : Promise.reject(new Error(response.statusText)))
     .then( response => response.text())
-    .then( text => {
-            const parser = new DOMParser();
-            const parsedXML = parser.parseFromString(text, 'text/xml');
-
-            if (parsedXML.getElementsByTagName('dataEntitity')[0].getAttribute('resultRecords') === '1') {
-                console.log({ id: parsedXML.getElementsByTagName('id')[0].childNodes[0].nodeValue,
-                    area: parsedXML.getElementsByTagName('area')[0].childNodes[0].nodeValue })
+    .then( text => convertFromTextToXML(text))
+    .then( parsedXML => {
+        dispatch(searchDone());
+        if (parsedXML.getElementsByTagName('dataEntitity')[0].getAttribute('resultRecords') === '1') {
+                console.log(createObjectFromXML(parsedXML))
             } else {
-                console.log(text)
+                console.log(parsedXML)
             }
         ;
-    }
-    );
-} 
-
-export function checkClickForCopper2(long, lat, callback) {
-
-    const sthlmPointUrlTemplate = 'https://us-central1-coppercors.cloudfunctions.net/copperProvider/?whatcopper=Geom=POINT(%longitude%%20%latitude%)';
-
-    const [x,y] = convertPoint(long, lat);
-
-    const request = new XMLHttpRequest();
-
-    request.open('GET', createUrl(sthlmPointUrlTemplate, x, y), true);
-    request.send();
-    request.addEventListener('readystatechange', processRequest, false);
-
-    function processRequest(e) {
-
-        if (request.readyState === 4 && request.status === 200) {
-            const parser = new DOMParser();
-            const response = parser.parseFromString(request.responseText, 'text/xml');
-
-            if (response.getElementsByTagName('dataEntitity')[0].getAttribute('resultRecords') === '1') {
-                callback({ id: response.getElementsByTagName('id')[0].childNodes[0].nodeValue,
-                    area: response.getElementsByTagName('area')[0].childNodes[0].nodeValue })
-            } else {
-                callback(null)
-            }
         }
-    }
+    );
 }
+
 
 function createUrl(templateURL, long, lat) {
     var queryURL = templateURL.replace('%longitude%', long);
