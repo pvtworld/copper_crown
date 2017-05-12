cfa6b6204af63a1724f27757ed8f04c2518fdf50
nij0 <niclas.j@bktv.nu>
2017-05-12 18:08:10 +0200

    Created actions and reducers and added to combiner

--- src/Redux/Reducers/combiner.jsx
+++ src/Redux/Reducers/combiner.jsx
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import { firebaseStateReducer } from 'react-redux-firebase'
 import { copperSearchReducer, displayRoofReducer, loginReducer} from './copperMapReducers';
+import { showModalReducer } from './navigationReducers';
 
 const reducers = combineReducers({
     login: loginReducer,
     firebase: firebaseStateReducer,
     copperSearch: copperSearchReducer,
     copperRoof: displayRoofReducer,
+    showModal: showModalReducer
 });
 
 export default reducers;
