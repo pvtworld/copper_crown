7cbe8a719aa7514e6e97c4862899c0a6d12504aa
Ludvig Lundberg <ludvig.lundberg@hotmail.com>
2017-05-15 09:08:14 +0200

    added redirect when logged in

--- src/Components/LoginContainer/LoginContainer.jsx
+++ src/Components/LoginContainer/LoginContainer.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Login from '../Login/Login'
 import './LoginContainer.css'
 import Loadable from 'react-loading-overlay'
+import { browserHistory } from 'react-router';
+import { pathToJS } from 'react-redux-firebase'
 import { connect } from 'react-redux'
 
 class LoginContainer extends React.Component {
 
     render() {
+        if(this.props.auth){
+            browserHistory.push('/app')
+        }
+
         return (
             <Loadable active={this.props.loadingUser} spinner>
             <div className="fullscreen">
@@ -25,9 +31,11 @@ class LoginContainer extends React.Component {
 
 const mapStateToProps = (state) => {
     return{
+        auth: pathToJS(state.firebase, 'auth'),
         loadingUser: state.login.loadingUser,
     }
 };
+
 export default connect(mapStateToProps)(LoginContainer)
 
 
