507e505a44b2825950ca45d4ce53272f6df32f94
nij0 <niclas.j@bktv.nu>
2017-05-16 15:56:35 +0200

    timer renders and sets state without errors

--- src/Components/DeadlineClock/DeadlineClock.jsx
+++ src/Components/DeadlineClock/DeadlineClock.jsx
@@ -1,29 +1,54 @@
 import React from 'react'
 
+const gameFinishDate = "May 16, 2017 16:00:30";
+
 export default class DeadlineClock extends React.Component{
 
-    componentDidMount(){
-        let countDownFromDate = new Date("May 16, 2017 13:07:00").getTime(); //When timer should end
-        let timeout = setInterval(countDownFunc, 1000);
-
-        function countDownFunc() {
-            let remaining = countDownFromDate - new Date().getTime();
-            convertToDayFormat((Math.max(0, remaining)));
-            if (remaining <= 0) {
-                clearInterval(timeout);
-                console.log('Timer finished!');
-            }
-        }
-        function convertToDayFormat(remaining){
-            let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
-            let hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-            let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
-            console.log('Days: '+ days + '\nHours: ' + hours + '\nMinutes: ' + minutes + '\nSeconds: ' + seconds);
+    constructor() {
+        super();
+        this.state = {
+            isMounted: false,
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
         }
     }
 
+    timer = null;
+
+    componentWillMount(){
+        this.createTimer()
+    }
+
+    componentDidMount(){
+        this.timer = setInterval(() => this.createTimer(), 1000)
+        this.setState({
+            isMounted: true
+        })
+    }
+
+    createTimer() {
+        let countDownFromDate = new Date(gameFinishDate).getTime(); //When timer should end
+        let remaining = countDownFromDate - new Date().getTime();
+
+        let days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+
+        if(this.state.isMounted){
+            this.setState({days, hours, minutes, seconds});
+        }
+        
+        if(remaining <= 1000){
+            clearInterval(this.timer);
+            console.log('FINISHED')
+        }
+
+
+    }
     render(){
-        return <h1></h1>;
+        return <h1> Days: {this.state.days} Hours: {this.state.hours} Minutes: {this.state.minutes} Seconds: {this.state.seconds}</h1>;
     }
 }
