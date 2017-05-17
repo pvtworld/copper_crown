6c05e126825d8f14e59673b2783f65f6f721e9b7
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-05-17 13:12:15 +0200

    small changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { pathToJS } from 'react-redux-firebase';
+import {firebaseConnect, pathToJS, dataToJS} from 'react-redux-firebase'
 import GameContainer from '../GameContainer/GameContainer';
 import TeamChooser from '../TeamChooser/TeamChooser';
 import { browserHistory } from 'react-router'
 import { getPricePerSquareMeter, bindPriceMultiplier } from '../../Helpers/PointsHelpers';
+
 
 class App extends React.Component {
 
