75e1bc79beb452c3fa4c354a0e47a57473a3ed44
nij0 <niclas.j@bktv.nu>
2017-05-23 10:56:09 +0200

    Added material spinner cuz more config :))

--- src/Components/InfoContainer/InfoContainer.jsx
+++ src/Components/InfoContainer/InfoContainer.jsx
@@ -4,19 +4,30 @@ import RoofInfo from "../RoofInfo/RoofInfo";
 import RoofStolen from '../RoofInfo/RoofStolen';
 import RoofNotFound from '../RoofInfo/RoofNotFound';
 import Spinner from 'react-spinkit';
-
+import RefreshIndicator from 'material-ui/RefreshIndicator';
 import PlayerInfo from '../PlayerInfo/PlayerInfo';
 import Leaderboard from '../Leaderboard/LeaderComponent';
 import GameInfo from '../GameInfoComponent/GameInfoComponent';
 import Profile from '../Profile/ProfileComponent'
 
 class InfoContainer extends React.Component {
-
     render() {
 
         if(this.props.searching){
             return(
-                <Spinner spinnerName="chasing-dots" noFadeIn />
+                <div className="loading-spinner">
+                    <RefreshIndicator
+                        size={60}
+                        left={-30}
+                        top={-30}
+                        loadingColor="#FF9800"
+                        status="loading"
+                        style={{
+                            display: 'inline-block',
+                            position: 'relative',
+                        }}
+                    />
+                </div>
             )
         }
 
