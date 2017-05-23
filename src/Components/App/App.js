92164904b292ac7c69d012767ead8437b8c85867
nij0 <niclas.j.jonsson@gmail.com>
2017-05-23 21:44:09 +0200

    Added to App and GameContiner

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,18 +5,45 @@ import GameContainer from '../GameContainer/GameContainer';
 import TeamChooser from '../TeamChooser/TeamChooser';
 import { browserHistory } from 'react-router'
 import { getPricePerSquareMeter, bindPriceMultiplier } from '../../Helpers/PointsHelpers';
-
+import * as pubnub from 'pubnub';
 
 class App extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            userID: Math.round(Math.random() * 1000000).toString(),
+            history: []
+        };
+    }
+
+    sendMessage = (message) => {
+        this.PubNub.publish({
+            channel: 'ReactChat',
+            message: message,
+        });
+    }
 
     componentDidMount(){
         getPricePerSquareMeter(this.props.dispatch);
         bindPriceMultiplier(this.props.dispatch);
 
+        this.PubNub = pubnub.init({
+            publish_key: 'pub-c-425d3d2e-ad86-4961-bb14-1eb59efec74d',
+            subscribe_key: 'sub-c-39bab7dc-3fcc-11e7-b6a4-02ee2ddab7fe',
+            ssl: (location.protocol.toLowerCase() === 'https:'),
+        });
+
+        this.PubNub.subscribe({
+            channel: 'ReactChat',
+            message: (message) => this.setState({
+                history: this.state.history.concat(message)
+            }),
+        });
+
     }
 
     redirectIfAuth = (props) => {
-
         if(!props.auth){
             console.log('User is not auth, redirecting to login /', props.auth);
             browserHistory.push('/')
@@ -28,11 +55,14 @@ class App extends React.Component {
 
     render() {
         this.redirectIfAuth(this.props);
-        // check if they are no logged in at all
         console.log('Creating GameContainer');
-        return ( <div>
+        return (
+            <div>
                 <TeamChooser/>
-                <GameContainer/>
+                <GameContainer history={this.state.history}
+                               userID={this.state.userID}
+                               sendMessage={this.sendMessage}
+                />
             </div>
         )
     }
--- src/Components/GameContainer/GameContainer.jsx
+++ src/Components/GameContainer/GameContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ScoreComponent from '../ScoreComponent/ScoreComponent'
 import CopperMap from '../CopperMap/CopperMap';
 import Navigationbar from '../Navigation/Navigationbar';
+import ChatComponent from '../ChatComponent/ChatComponent'
 
 class GameContainer extends React.Component{
 
@@ -9,6 +10,10 @@ class GameContainer extends React.Component{
         return (
             <div>
                 <Navigationbar/>
+                <ChatComponent history={this.props.history}
+                               userID={this.props.userID}
+                               sendMessage={this.props.sendMessage}
+                />
                 <ScoreComponent/>
                 <CopperMap/>
             </div>
