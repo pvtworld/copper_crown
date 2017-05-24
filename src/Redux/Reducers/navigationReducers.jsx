4f5df317b4d631efbf3fff1676dc41bd2d3bf5fe
nij0 <niclas.j@bktv.nu>
2017-05-24 13:05:14 +0200

    chat nav reducer

--- src/Redux/Reducers/navigationReducers.jsx
+++ src/Redux/Reducers/navigationReducers.jsx
@@ -10,6 +10,8 @@ export var showModalReducer = (state = {showModalString: ''}, action) => {
             return {...state, showModalString: 'SHOW_PROFILE'};
         case 'RESET_MODAL':
             return {...state, showModalString: 'RESET_MODAL'};
+        case 'SHOW_GAME_CHAT':
+            return {...state, showModalString: 'SHOW_GAME_CHAT'};
         default:
             return state;
     }
