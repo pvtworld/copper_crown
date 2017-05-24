46efaf13f60cb1a67111402182def3365d876777
tiliacordata <tilia_cordata@outlook.com>
2017-05-24 12:32:19 +0200

    Removed viewing of username chooser on startup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,60 +2,38 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {pathToJS} from 'react-redux-firebase'
 import GameContainer from '../GameContainer/GameContainer';
-import TeamChooser from '../TeamChooser/TeamChooser';
+//import TeamChooser from '../TeamChooser/TeamChooser';
 import { browserHistory } from 'react-router'
 import { getPricePerSquareMeter, bindPriceMultiplier } from '../../Helpers/PointsHelpers';
 
 
 class App extends React.Component {
 
-    constructor(){
-        super();
-        this.handleClose = this.handleClose.bind(this);
-        this.state = {
-            showUsernameModal: true
-        }
-    }
-
-    componentDidMount(){
+    componentDidMount() {
         getPricePerSquareMeter(this.props.dispatch);
         bindPriceMultiplier(this.props.dispatch);
-
     }
 
     redirectIfAuth = (props) => {
 
-        if(!props.auth){
+        if (!props.auth) {
             console.log('User is not auth, redirecting to login /', props.auth);
             browserHistory.push('/')
-        }else{
+        } else {
             console.log('User is auth, doing nothing');
 
         }
-}
-
-    handleClose(){
-        this.setState({
-        showUsernameModal: false}
-    )};
-
+    }
 
     render() {
         this.redirectIfAuth(this.props);
         // check if they are no logged in at all
         console.log('Creating GameContainer');
-        if (this.state.showUsernameModal){
-            return ( <div>
-                    <TeamChooser onClose={this.handleClose}/>
-                    <GameContainer/>
-                </div>
-            )
-        } else {
-            return ( <div>
-                    <GameContainer/>
-                </div>
-            )
-        }
+
+        return ( <div>
+                <GameContainer/>
+            </div>
+        )
     }
 }
 
