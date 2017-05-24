655a62d549620ec3540af5bee81239b2e6bd8989
Ludvig Lundberg <ludvig.lundberg@hotmail.com>
2017-05-24 12:02:23 +0200

    added ability for user to remove roof

--- src/Components/PlayerInfo/PlayerInfo.jsx
+++ src/Components/PlayerInfo/PlayerInfo.jsx
@@ -1,43 +1,69 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { firebaseConnect, dataToJS, pathToJS } from 'react-redux-firebase';
-import {ListGroup, ListGroupItem} from 'react-bootstrap';
-import { Button, Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+import { List, ListItem } from 'material-ui';
+import DeleteForever from 'material-ui/svg-icons/action/delete-forever';
+import {red500} from 'material-ui/styles/colors';
 import { resetModal } from '../../Redux/Actions/navigationActions'
 
+const createRoofs = (roofs) => {
+    const objectKeys = Object.keys(roofs);
+    const array = Object.values(roofs);
+    for (let i = 0; i < array.length; i++) { 
+        (array[i]).firebaseId = objectKeys[i];
+    }
+
+    console.log(array)
+    return array;
+}
+
+const removeRoof = (roof,firebase) => () => {
+    firebase.remove(`stolenRoofs/${roof.firebaseId}`)
+}
+
 const PlayerInfo = (props) => {
-
+    
         return (
-            <div className="static-modal">
-                <Modal.Dialog>
-                    <Modal.Header>
-                        <Modal.Title>Player Info</Modal.Title>
-                    </Modal.Header>
+            <div>
+        <Modal.Dialog>
+      <Modal.Header>
+        <Modal.Title>Stolen Roofs</Modal.Title>
+      </Modal.Header>
 
-                    <Modal.Body>
-                        <ListGroup>
-                            <ListGroupItem bsStyle="success">PlayerID: {props.auth.uid} </ListGroupItem>
-                            <ListGroupItem bsStyle="info">Value of copper stolen: {props.userInfo.points || 0 + ' kr'}</ListGroupItem>
-                            <ListGroupItem bsStyle="warning">Area of copper stolen: {props.userInfo.areaOfCopper || 0 + ' cm2'}</ListGroupItem>
-                        </ListGroup>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button bsStyle="primary" onClick={() => props.dispatch(resetModal())}>OK</Button>
-                    </Modal.Footer>
+      <Modal.Body>
+        <List>
+            {props.stolenRoofs ? 
+            <div> 
+                {
+                createRoofs(props.stolenRoofs).map( roof => (<ListItem className="roof" key={roof.roofId} primaryText={roof.roofId} disabled={true} rightIcon={<DeleteForever hoverColor={red500} onClick={removeRoof(roof, props.firebase)}/>}> </ListItem>)) 
+                }
+            </div> 
+                : <div> No roofs stolen </div>
+            }
+        </List>
+      </Modal.Body>
 
-                </Modal.Dialog>
+      <Modal.Footer>
+        <Button bsStyle="primary" onClick={() => props.dispatch(resetModal())}>Close</Button>
+      </Modal.Footer>
+
+        </Modal.Dialog>
             </div>
         )
     }
 
-const mapStateToProps = ({firebase}, {auth}) => ({
-    userInfo: auth ? dataToJS(firebase, `users/${auth.uid}`) : undefined
-})
+
+const mapStateToProps = (state) => {
+    return{
+        stolenRoofs: dataToJS(state.firebase, `stolenRoofs`),
+    }
+}
 
 const propsConnected = connect(mapStateToProps)(PlayerInfo)
 
 const wrappedPlayerInfo = firebaseConnect(
-    ({auth}) => ([auth ? `users/${auth.uid}`: '/']))(propsConnected);
+    ({auth}) => ([auth ? `stolenRoofs#orderByChild=userId&equalTo=${auth.uid}`: '/']))(propsConnected);
 
 const authConnected = connect(
  ({ firebase }) => ({
@@ -45,5 +71,5 @@ const authConnected = connect(
   })
 )(wrappedPlayerInfo)
 
-export default authConnected;
+export default authConnected
 
