12de3c4da2b2895a2dc8f28a8b80494e440dcc6f
Daniel Andersson <lars.daniel.andersson@gmail.com>
2017-05-25 11:24:35 +0200

    add create default user values on first login

--- src/Components/GameContainer/GameContainer.jsx
+++ src/Components/GameContainer/GameContainer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {firebaseConnect, pathToJS, dataToJS} from 'react-redux-firebase'
 import ScoreComponent from '../ScoreComponent/ScoreComponent'
 import CopperMap from '../CopperMap/CopperMap';
 import Navigationbar from '../Navigation/Navigationbar';
@@ -6,6 +8,15 @@ import Navigationbar from '../Navigation/Navigationbar';
 class GameContainer extends React.Component{
 
     render() {
+        if (!this.props.userInfo) {
+            this.props.dispatch({type: 'CREATING_DEFAULT_USER_VALUES'})
+            this.props.firebase.set(`users/${this.props.uid}`, {points: 0, areaOfCopper: 0, roofsStolen: 0, userName: this.props.userName})
+                .then(() => {
+                    this.props.dispatch({type: 'CREATED_DEFAULT_USER_VALUES'})
+                    return Promise.resolve();
+                })
+        }
+
         return (
             <div>
                 <Navigationbar/>
@@ -15,4 +26,24 @@ class GameContainer extends React.Component{
         )
     }
 }
-export default GameContainer;
+
+const mapStateToProps = (state, {auth}) => {
+    return{
+        userInfo: dataToJS(state.firebase, `users/${auth.uid}`),
+        userName: auth.displayName,
+        uid : auth.uid
+    }
+}
+
+const propsConnected = connect(mapStateToProps)(GameContainer)
+
+const wrappedPlayerInfo = firebaseConnect(
+    ({auth}) => ([auth ? `users/${auth.uid}` : '/']))(propsConnected);
+
+const authConnected = connect(
+    ({ firebase }) => ({
+        auth: pathToJS(firebase, 'auth')
+    })
+)(wrappedPlayerInfo)
+
+export default authConnected
