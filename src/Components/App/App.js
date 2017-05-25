a34e8a7b7e5a486b51bb8227a2d781f2a672bd44
nij0 <niclas.j@bktv.nu>
2017-05-25 09:35:59 +0200

    removed unnecessary code from app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,38 +9,9 @@ import { getPricePerSquareMeter, bindPriceMultiplier } from '../../Helpers/Point
 
 class App extends React.Component {
 
-    constructor(){
-        super();
-/*        this.state = {
-            userID: Math.round(Math.random() * 1000000).toString(),
-            history: []
-        };*/
-    }
-
-/*    sendMessage = (message) => {
-    this.PubNub.publish({
-        channel: 'ReactChat',
-        message: message,
-    });
-}*/
-
     componentDidMount(){
         getPricePerSquareMeter(this.props.dispatch);
         bindPriceMultiplier(this.props.dispatch);
-
-        /*this.PubNub = pubnub.init({
-            publish_key: 'pub-c-425d3d2e-ad86-4961-bb14-1eb59efec74d',
-            subscribe_key: 'sub-c-39bab7dc-3fcc-11e7-b6a4-02ee2ddab7fe',
-            ssl: (location.protocol.toLowerCase() === 'https:'),
-        });
-
-        this.PubNub.subscribe({
-            channel: 'ReactChat',
-            message: (message) => this.setState({
-                history: this.state.history.concat(message)
-            }),
-        });*/
-
     }
 
     redirectIfAuth = (props) => {
