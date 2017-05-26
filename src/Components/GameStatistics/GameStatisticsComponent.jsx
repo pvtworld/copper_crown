4db07954c7af32966f242ae03817cd9b3dbf7185
MoaEkberg <chipanmoa@hotmail.com>
2017-05-26 11:50:47 +0200

    gamestatistics modified to being a class

--- src/Components/GameStatistics/GameStatisticsComponent.jsx
+++ src/Components/GameStatistics/GameStatisticsComponent.jsx
@@ -4,25 +4,72 @@ import { firebaseConnect, pathToJS, dataToJS} from 'react-redux-firebase';
 import { Button, Modal } from 'react-bootstrap';
 import { resetModal } from '../../Redux/Actions/navigationActions';
 import DeadlineClock from '../DeadlineClock/DeadlineClock'
+import LinearProgress from 'material-ui/LinearProgress';
 
-const GameStatisticsComponent = (props) => {
+var allRoofs = 11907;
+class GameStatisticsComponent extends React.Component {
 
-    const allRoofs = 11907;
-    
-    var roofArray = props.stolenRoofs ? Object.keys(props.stolenRoofs): [];
-    var roofsStolen = roofArray.length;
+      constructor(props) {
+    super(props);
 
-    var playerArray = props.users? Object.keys(props.users): [];
-    var numOfPlayers = playerArray.length;
+    this.state = {
+      completed: 0,
+    };
+  }
 
-    var percentRoofsLeft = ((allRoofs - roofsStolen) / allRoofs) * 100;
+    componentDidMount() {
+    this.timer = setTimeout(() => this.progress(5), 1000);
+  }
 
-    var squareMeterPrice = props.copperPrice ? props.copperPrice : 0;
-    var multiplier = props.copperMultiplier ? props.copperMultiplier : 0;
-    var copperPrice = squareMeterPrice * multiplier;
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
 
+  progress(completed) {
+    if (completed > 100) {
+      this.setState({completed: 100});
+    } else {
+      this.setState({completed});
+      const diff = Math.random() * 10;
+      this.timer = setTimeout(() => this.progress(completed + diff), 1000);
+    }
+  } 
+
+  
+
+    numOfRoofsLeft = (props) =>{
+        console.log("Detta är props1: " + props)
+        var roofArray = props.stolenRoofs ? Object.keys(props.stolenRoofs): [];
+        var roofsStolen = roofArray.length; 
+
+        return(
+        allRoofs-roofsStolen
+        )
+    }
+
+    percentOfRoofsLeft = (props) =>{
+        return(
+            (this.numOfRoofsLeft(props)/allRoofs)*100
+        )
+    }
+
+    numOfPlayers = (props) =>{
+        var playerArray = props.users? Object.keys(props.users): [];
+        return(playerArray.length
+        )
+    }
+
+    countDailyCopperPrice = (props) =>{
+        var squareMeterPrice = props.copperPrice ? props.copperPrice : 0;
+        var multiplier = props.copperMultiplier ? props.copperMultiplier : 0;
+        return(squareMeterPrice * multiplier)
+    } 
+
+render(){
+    console.log("Detta är props2: " + this.props);
     return (
         <div className="static-modal">
+            <LinearProgress mode="determinate" value={this.state.completed} />
             <Modal.Dialog>
                 <Modal.Header>
                     <Modal.Title>Game statistics</Modal.Title>
@@ -33,19 +80,19 @@ const GameStatisticsComponent = (props) => {
                     <DeadlineClock/>
                     <br></br>
                     <h5>Total number of roofs: {allRoofs}</h5>
-                    <h5>Number of roofs left: {allRoofs - roofsStolen}</h5>
-                    <h5>Percent of roofs left: {percentRoofsLeft.toFixed(1) + "%"}</h5>
+                    <h5>Number of roofs left: {this.numOfRoofsLeft(this.props)}</h5>
+                    <h5>Percent of roofs left: {this.percentOfRoofsLeft(this.props).toFixed(1) + "%"}</h5>
                     <br></br>
-                    <h5>Current number of players: {numOfPlayers}</h5>
-                    <h5>Current copper price: {copperPrice.toFixed(2) + " kr/sqm"}</h5>
+                    <h5>Current number of players: {this.numOfPlayers(this.props)}</h5>
+                    <h5>Current copper price: {this.countDailyCopperPrice(this.props).toFixed(2) + " kr/sqm"}</h5>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button bsStyle="primary" onClick={() => props.dispatch(resetModal())}>OK</Button>
+                    <Button bsStyle="primary" onClick={() => this.props.dispatch(resetModal())}>OK</Button>
                 </Modal.Footer>
 
             </Modal.Dialog>
         </div>
-    )
+    );}
 }
 
 
