051b3880cbf497812404a3ef050196da6d24fc59
Murmellow <max.lund.jonsson@gmail.com>
2017-05-29 12:02:14 +0200

    Updated Map colouring

--- src/Components/CopperMap/CopperMap.js
+++ src/Components/CopperMap/CopperMap.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { geoError, geoOptions} from '../../Helpers/GeoHelpers';
 import InfoContainer from "../InfoContainer/InfoContainer";
 import GameMap from "../GameMap/GameMap";
-import _ from "lodash";
+//import _ from "lodash";
 
 
 class CopperMap extends Component {
@@ -21,31 +21,31 @@ class CopperMap extends Component {
 
     geoLocationWatcher = this.geoLocationWatcher.bind(this);
 
-initColourMap() {
-    var layer = new FusionTablesLayer({
-          query: {
-            select: 'geometry',
-            from: '1ertEwm-1bMBhpEwHhtNYT47HQ9k2ki_6sRa-UQ'
-          },
-          styles: [{
-            polygonOptions: {
-              fillColor: '#00FF00',
-              fillOpacity: 0.3
-            }
-          }, {
-            where: 'birds > 300',
-            polygonOptions: {
-              fillColor: '#0000FF'
-            }
-          }, {
-            where: 'population > 5',
-            polygonOptions: {
-              fillOpacity: 1.0
-            }
-          }]
-        });
-        layer.setMap(map);
-      }
+// initColourMap() {
+//     var layer = new FusionTablesLayer({
+//           query: {
+//             select: 'geometry',
+//             from: '1ertEwm-1bMBhpEwHhtNYT47HQ9k2ki_6sRa-UQ'
+//           },
+//           styles: [{
+//             polygonOptions: {
+//               fillColor: '#00FF00',
+//               fillOpacity: 0.3
+//             }
+//           }, {
+//             where: 'birds > 300',
+//             polygonOptions: {
+//               fillColor: '#0000FF'
+//             }
+//           }, {
+//             where: 'population > 5',
+//             polygonOptions: {
+//               fillOpacity: 1.0
+//             }
+//           }]
+//         });
+//         layer.setMap(map);
+//       }
     
 
     geoLocationWatcher() {
